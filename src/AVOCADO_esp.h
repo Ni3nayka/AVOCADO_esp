1220eda2e9d92c52187f44f396cb38e4f5c23b91
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-11-07 21:56:00 +0300

    clone from Ni3nayka/wifi_boot_esp

---
+++ src/AVOCADO_esp.h
@@ -0,0 +1,12 @@
+/*
+   Write for pack lib AVOCADO_esp:
+   https://github.com/Ni3nayka/AVOCADO_esp
+
+   author: Egor Bakay <egor_bakay@inbox.ru>
+   write:  november 2022
+   modify: november 2022
+*/
+
+#include "OTA.h"
+
+OTA wifi_boot_esp;
