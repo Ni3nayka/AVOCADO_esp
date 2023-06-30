cfffe37b25ac8f1681a48b005249faa6d8131687
Egor Bakay <egor_bakay@inbox.ru>
2023-06-30 23:20:39 +0300

    Update monitor.h

--- src/monitor.h
+++ src/monitor.h
@@ -23,7 +23,12 @@
 #error "You not defined SERVER_PORT"
 #endif
 
-#include <ESP8266WiFi.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#endif
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
 
 WiFiClient client;
 
